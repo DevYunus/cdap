51733677637231fa142eae130754cfac7da68b66
Ali Anwar <anwar1@berkeley.edu>
2016-11-09 11:32:31 -0800

    Fix test case

--- cdap-app-fabric/src/test/java/co/cask/cdap/internal/app/services/http/AuthorizationBootstrapperTest.java
+++ cdap-app-fabric/src/test/java/co/cask/cdap/internal/app/services/http/AuthorizationBootstrapperTest.java
@@ -117,6 +117,8 @@ public class AuthorizationBootstrapperTest {
     cConf.set(Constants.AppFabric.SYSTEM_ARTIFACTS_DIR, systemArtifactsDir.getAbsolutePath());
     createSystemArtifact(systemArtifactsDir);
     Injector injector = Guice.createInjector(new AppFabricTestModule(cConf));
+    namespaceQueryAdmin = injector.getInstance(NamespaceQueryAdmin.class);
+    namespaceAdmin = injector.getInstance(NamespaceAdmin.class);
     defaultNamespaceEnsurer = new DefaultNamespaceEnsurer(namespaceAdmin);
     discoveryServiceClient = injector.getInstance(DiscoveryServiceClient.class);
     txManager = injector.getInstance(TransactionManager.class);
@@ -125,8 +127,6 @@ public class AuthorizationBootstrapperTest {
     authorizationEnforcementService.startAndWait();
     systemArtifactLoader = injector.getInstance(SystemArtifactLoader.class);
     authorizationBootstrapper = injector.getInstance(AuthorizationBootstrapper.class);
-    namespaceQueryAdmin = injector.getInstance(NamespaceQueryAdmin.class);
-    namespaceAdmin = injector.getInstance(NamespaceAdmin.class);
     artifactRepository = injector.getInstance(ArtifactRepository.class);
     dsFramework = injector.getInstance(DatasetFramework.class);
   }
